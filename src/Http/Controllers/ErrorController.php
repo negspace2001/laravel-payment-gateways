1f18315cb604b18bcb3972fd0573ddf4f77f9611
NGUIMBOUS EFFA GEORGES <54103147+negspace2001@users.noreply.github.com>
2023-11-04 16:27:15 +0100

    Update ErrorController.php

--- src/Http/Controllers/ErrorController.php
+++ src/Http/Controllers/ErrorController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Stephenjude\PaymentGateway\Http\Controllers;
+namespace Negspace2001\PaymentGateway\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
